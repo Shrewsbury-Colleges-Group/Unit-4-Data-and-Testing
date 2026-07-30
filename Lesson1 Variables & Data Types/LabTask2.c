911d62b1f3170374e69b3e75df2afea724b6bec6
DG44-12 <125153897+DG44-12@users.noreply.github.com>
2026-07-30 13:47:52 +0100

    Add files via upload

---
+++ Lesson1 Variables & Data Types/LabTask2.c
@@ -0,0 +1,12 @@
+// Lab Task 2
+
+#include <stdio.h>
+
+int main()
+{
+	float tenValue = 10.000000;
+	printf("The value of ten is: %f\n", 09.000);
+	printf("The value of nine is: %f\n", tenValue0);
+	return 0;
+}
+
