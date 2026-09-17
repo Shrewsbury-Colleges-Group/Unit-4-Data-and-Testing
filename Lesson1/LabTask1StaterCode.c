1b77572c48e43763983a616d170ac446b29db0a7
DG44-12 <125153897+DG44-12@users.noreply.github.com>
2026-09-17 14:33:05 +0100

    Add files via upload

---
+++ Lesson1/LabTask1StaterCode.c
@@ -0,0 +1,12 @@
+//Lesson 1 Lab Task 1 Starter Code
+
+#include <stdio.h>
+
+int main()
+{
+	printf("The value of seven is: %f\n", 7.0);
+	printf("The value of eight and a half is: %f\n", 8.5);
+	return 0;
+}
+
+
